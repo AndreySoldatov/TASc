80c0b4d595501ca3fdafb7379f847669b2ed65f4
AndreySoldatov <SoldatovAndreyWork@gmail.com>
2023-04-09 13:34:55 +0300

    started working on client

--- test.c
+++ test.c
@@ -3,20 +3,5 @@
 #include "tas_net.h"
 
 int main() {
-    Server s = netServerNew(8080);
-
-    while(1) {
-        netServerAcceptAvailableClient(&s);
-
-        Vec_ServerMessage messages = netServerGetMessages(&s);
-
-        for (size_t i = 0; i < messages.length; i++) {
-            strPrintln(messages.data[i].msg);
-            netServerMessageDelete(&messages.data[i]);
-        }
-
-        vecDelete_ServerMessage(&messages);
-
-        sleep(1);
-    }
+    //TODO: client
 }
